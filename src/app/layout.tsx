ba5e0d4a32f1d9d0f09da1d1f2dc4f93f79f2f75
Sarmad Irfan <sarmadirfan@Sarmads-MacBook-Pro.local>
2025-04-29 21:01:27 +0500

    added SEO 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import {Sora} from "next/font/google"
+import { Sora } from "next/font/google"
 import "./globals.css";
 import Providers from "@/containers/providers";
 import Navbar from "@/components/navbar";
@@ -8,9 +8,9 @@ import ThemeContextProvider from "@/components/theme-context";
 import Script from "next/script";
 
 const sora = Sora({
-  subsets:['latin'],
-  variable:'--font-sora',
-  weight:['100','200','300','400','500','600','700','800']
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
 })
 
 export const metadata: Metadata = {
@@ -63,35 +63,37 @@ export default function RootLayout({
             gtag('config', 'G-05BX1H3XYB');
           `}
         </Script>
-        <Script type="application/ld+json">
-{`
-  "@context": "https://schema.org",
-  "@type": "Person",
-  "name": "Sarmad Irfan",
-  "url": "https://sarmadirfan.com",
-  "sameAs": [
-    "https://www.linkedin.com/in/sarmad-irfan",
-    "https://github.com/sarmad0324"
-  ],
-  "jobTitle": "Web & App Developer",
-  "worksFor": {
-    "@type": "Software Agency",
-    "name": "Freelance / Sarmad Irfan Agency"
-  }`
-}
+        <Script id="structured-data" type="application/ld+json">
+  {`
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "name": "Sarmad Irfan",
+      "url": "https://sarmadirfan.com",
+      "sameAs": [
+        "https://www.linkedin.com/in/sarmad-irfan",
+        "https://github.com/sarmad0324"
+      ],
+      "jobTitle": "Web & App Developer",
+      "worksFor": {
+        "@type": "Software Agency",
+        "name": "Freelance / Sarmad Irfan Agency"
+      }
+    }
+  `}
 </Script>
         <link rel="icon" href="/favicon.ico" />
       </head>
       <ThemeContextProvider>
         <body
           className={`${sora.variable} font-Sora flex flex-col bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90  antialiased`}
-          >
+        >
           <div className="bg-[#fbe2e3] absolute top-[-6rem] flex-1 -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]" ></div>
           <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-[10] flex-1 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]  " ></div>
           <Providers>
-            <Navbar/>
+            <Navbar />
             {children}
-            <ThemeSwitch/>
+            <ThemeSwitch />
           </Providers>
         </body>
       </ThemeContextProvider>
